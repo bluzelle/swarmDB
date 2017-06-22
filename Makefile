9d2ce0e95047467e897f2b8e1e40d91b40094d45
Neeraj Murarka <njmurarka@gmail.com>
2017-06-22 21:43:05 +0800

    Added in support for boost libraries

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
-LIB_FLAGS =
+LIB_FLAGS = -L/usr/local/Cellar/boost/1.64.0_1/lib
+CPP_FLAGS = -I/usr/local/Cellar/boost/1.64.0_1/include
 ALL_OBJECTS = src/main.o
 
 all: $(ALL_OBJECTS)
-	g++ -o kepler $(ALL_OBJECTS) $(LIB_FLAGS)
+	g++ -o kepler $(ALL_OBJECTS) $(LIB_FLAGS) -lboost_locale-mt
+
+src/main.o: src/main.cpp
+	g++ -o src/main.o -c src/main.cpp -W -Wall -ansi $(CPP_FLAGS)
 
 clean:
-	rm -rf *.o kepler
+	rm -rf src/*.o kepler
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,32 @@
+#include <boost/locale.hpp>
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
 int main()
-	{
-	cout << "Hello world";
-	}
+    {
+    using namespace boost::locale;
+    using namespace std;
+    generator gen;
+    locale loc=gen(""); 
+    // Create system default locale
+
+    locale::global(loc); 
+    // Make it system global
+    
+    cout.imbue(loc);
+    // Set as default locale for output
+    
+    cout <<format("Today {1,date} at {1,time} we had run our first localization example") % time(0) 
+          <<endl;
+   
+    cout<<"This is how we show numbers in this locale "<<as::number << 103.34 <<endl; 
+    cout<<"This is how we show currency in this locale "<<as::currency << 103.34 <<endl; 
+    cout<<"This is typical date in the locale "<<as::date << std::time(0) <<endl;
+    cout<<"This is typical time in the locale "<<as::time << std::time(0) <<endl;
+    cout<<"This is upper case "<<to_upper("Hello World!")<<endl;
+    cout<<"This is lower case "<<to_lower("Hello World!")<<endl;
+    cout<<"This is title case "<<to_title("Hello World!")<<endl;
+    cout<<"This is fold case "<<fold_case("Hello World!")<<endl;
+    }
