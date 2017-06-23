cd8407717902671b38f3be8d78abbdd4d1a7e16d
Neeraj Murarka <njmurarka@gmail.com>
2017-06-23 15:38:04 +0800

    Added in GUI support with wxWidgets

--- Makefile
+++ Makefile
@@ -1,12 +1,31 @@
-LIB_FLAGS = -L/usr/local/Cellar/boost/1.64.0_1/lib
-CPP_FLAGS = -I/usr/local/Cellar/boost/1.64.0_1/include
-ALL_OBJECTS = src/main.o
+# Define default flags (include your source tree for example
 
-all: $(ALL_OBJECTS)
-	g++ -o kepler $(ALL_OBJECTS) $(LIB_FLAGS) -lboost_locale-mt
+CXXFLAGS = `wx-config --cxxflags` -I/usr/local/Cellar/boost/1.64.0_1/include
+LDLIBS   = `wx-config --libs` -L/usr/local/Cellar/boost/1.64.0_1/lib -lboost_locale-mt
 
-src/main.o: src/main.cpp
-	g++ -o src/main.o -c src/main.cpp -W -Wall -ansi $(CPP_FLAGS)
+ifdef FINAL
+    EXTRAFLAGS = -MD -O2 -fno-rtti -fno-exceptions -fomit-frame-pointer
+else
+    EXTRAFLAGS = -MD -g
+endif
+
+# Define our sources, calculate the dependecy files and object files
+
+KEPLER_SOURCES := src/main.cpp 
+KEPLER_OBJECTS := $(patsubst %.cpp, %.o, ${KEPLER_SOURCES})
+KEPLER_DEPS := $(patsubst %.cpp, %.d, ${KEPLER_SOURCES})
+
+#include our project dependecy files
+
+-include $(KEPLER_DEPS)
+
+all: kepler
+
+kepler: $(KEPLER_OBJECTS)
+	$(CXX) -o kepler $(KEPLER_OBJECTS) $(LDLIBS)
+ifdef FINAL
+	strip kepler
+endif
 
 clean:
-	rm -rf src/*.o kepler
+	rm -rf kepler.exe kepler $(KEPLER_OBJECTS) $(KEPLER_DEPS)
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,124 @@
 #include <boost/locale.hpp>
+
 #include <iostream>
+
 #include <ctime>
 
-using namespace std;
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
 
-int main()
+class KeplerApplication: public wxApp
+    {
+    public:
+
+        virtual bool OnInit();
+    };
+
+class KeplerFrame: public wxFrame
+    {
+    public:
+    
+        KeplerFrame();
+    
+    private:
+    
+        void OnKepler(wxCommandEvent& event);
+        void OnExit(wxCommandEvent& event);
+        void OnAbout(wxCommandEvent& event);
+    };
+
+enum
+    {
+    ID_Kepler = 1
+    };
+
+wxIMPLEMENT_APP(KeplerApplication);
+
+bool KeplerApplication::OnInit()
+    {
+    KeplerFrame *frame = new KeplerFrame();
+    
+    frame->Show(true);
+    
+    return true;
+    }
+
+KeplerFrame::KeplerFrame()
+            :wxFrame(NULL, 
+                     wxID_ANY, 
+                     "Kepler TestNet Node")
+    {
+    wxMenu *menuFile = new wxMenu;
+
+    menuFile->Append(ID_Kepler, 
+                     "&Welcome...\tCtrl-H",
+                     "Change this text to something appropriate");
+    
+    menuFile->AppendSeparator();
+    
+    menuFile->Append(wxID_EXIT);
+    
+    wxMenu *menuHelp = new wxMenu;
+    
+    menuHelp->Append(wxID_ABOUT);
+    
+    wxMenuBar *menuBar = new wxMenuBar;
+    
+    menuBar->Append(menuFile, 
+                    "&File");
+    
+    menuBar->Append(menuHelp, 
+                    "&Help");
+    
+    SetMenuBar(menuBar);
+    
+    CreateStatusBar();
+    
+    SetStatusText("Welcome to Kepler!");
+    
+    Bind(wxEVT_MENU, 
+         &KeplerFrame::OnKepler, 
+         this, 
+         ID_Kepler);
+    Bind(wxEVT_MENU, 
+         &KeplerFrame::OnAbout, 
+         this, 
+         wxID_ABOUT);
+    Bind(wxEVT_MENU, 
+         &KeplerFrame::OnExit, 
+         this, 
+         wxID_EXIT);
+    }
+
+void KeplerFrame::OnExit(wxCommandEvent& event)
+    {
+    Close(true);
+    }
+
+void KeplerFrame::OnAbout(wxCommandEvent& event)
     {
     using namespace boost::locale;
     using namespace std;
     generator gen;
-    locale loc=gen(""); 
+
     // Create system default locale
 
-    locale::global(loc); 
+    locale loc = gen(""); 
+
     // Make it system global
-    
-    cout.imbue(loc);
-    // Set as default locale for output
-    
-    cout <<format("Today {1,date} at {1,time} we had run our first localization example") % time(0) 
-          <<endl;
-   
-    cout<<"This is how we show numbers in this locale "<<as::number << 103.34 <<endl; 
-    cout<<"This is how we show currency in this locale "<<as::currency << 103.34 <<endl; 
-    cout<<"This is typical date in the locale "<<as::date << std::time(0) <<endl;
-    cout<<"This is typical time in the locale "<<as::time << std::time(0) <<endl;
-    cout<<"This is upper case "<<to_upper("Hello World!")<<endl;
-    cout<<"This is lower case "<<to_lower("Hello World!")<<endl;
-    cout<<"This is title case "<<to_title("Hello World!")<<endl;
-    cout<<"This is fold case "<<fold_case("Hello World!")<<endl;
+
+    locale::global(loc); 
+
+    const string str = (format("Today {1,date} at {1,time} we had run Kepler") % time(0)).str();
+
+    wxMessageBox(str,
+                 "About Kepler", 
+                 wxOK | wxICON_INFORMATION);
+    }
+
+void KeplerFrame::OnKepler(wxCommandEvent& event)
+    {
+    wxLogMessage("Hello from Kepler!");
     }
