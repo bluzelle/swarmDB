c1628096c024e060ced5746cb419b04c8fbf5537
Neeraj Murarka <njmurarka@gmail.com>
2017-07-18 01:40:12 -0700

    Added test controls to verify layouts of Kepler program

--- src/main.cpp
+++ src/main.cpp
@@ -90,6 +90,33 @@ KeplerFrame::KeplerFrame()
          &KeplerFrame::OnExit, 
          this, 
          wxID_EXIT);
+
+    wxBoxSizer *topsizer = new wxBoxSizer( wxVERTICAL );
+    // create text ctrl with minimal size 100x60
+    topsizer->Add(
+        new wxTextCtrl( this, -1, "My text.", wxDefaultPosition, wxSize(100,60), wxTE_MULTILINE),
+        1,            // make vertically stretchable
+        wxEXPAND |    // make horizontally stretchable
+        wxALL,        //   and make border all around
+        10 );         // set border width to 10
+    wxBoxSizer *button_sizer = new wxBoxSizer( wxHORIZONTAL );
+    button_sizer->Add(
+        new wxButton( this, wxID_OK, "OK" ),
+        0,           // make horizontally unstretchable
+        wxALL,       // make border all around (implicit top alignment)
+        10 );        // set border width to 10
+    button_sizer->Add(
+        new wxButton( this, wxID_CANCEL, "Cancel" ),
+        0,           // make horizontally unstretchable
+        wxALL,       // make border all around (implicit top alignment)
+        10 );        // set border width to 10
+    topsizer->Add(
+        button_sizer,
+        0,                // make vertically unstretchable
+        wxALIGN_CENTER ); // no border and centre horizontally
+    SetSizerAndFit(topsizer); // use the sizer for layout and size window
+                              // accordingly and prevent it from being resized
+                              // to smaller size
     }
 
 void KeplerFrame::OnExit(wxCommandEvent& event)
