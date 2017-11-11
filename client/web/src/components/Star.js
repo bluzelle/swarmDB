99fd269ef7b40f28eeb4354376363d64db7495dd
scott <scott@bulldoginfo.com>
2017-11-10 18:41:06 -0800

    added star for leader node

---
+++ client/web/src/components/Star.js
@@ -0,0 +1,26 @@
+import range from 'lodash/range'
+
+const Star = ({cx, cy, r}) => {
+    const points = range(0,10)
+        .map(i => (Math.PI * 2 / 10) * i)
+        .map((a, idx) => {
+        const offset = (((idx % 2) ? .5 : 1) * r) / 2;
+        return [Math.cos(a) * offset + cx, Math.sin(a) * offset + cy]})
+
+        .map(([x, y]) => `${x},${y}`)
+        .join(' ');
+    console.log(points);
+    // const angle = ((Math.PI * 2 / nodes.length) * idx);
+    // return {
+    //     xAngle: Math.cos(angle),
+    //     yAngle: Math.sin(angle),
+    //     ...node
+    // }
+
+    return (
+        <polygon points={points} fill="white" />
+    );
+
+};
+
+export default Star
--- client/web/src/components/tabs/NodeGraph/Node.js
+++ client/web/src/components/tabs/NodeGraph/Node.js
@@ -1,4 +1,5 @@
 import nodeColors from 'constants/nodeStatusColors'
+import Star from 'components/Star'
 
 const Node = ({node, onMouseOver, selected}) => {
     const {address, nodeState, xAngle, yAngle} = node;
@@ -6,16 +7,7 @@ const Node = ({node, onMouseOver, selected}) => {
     const cy = 90 * yAngle + 100;
 
     const r = new Date().getTime() - node.lastMessageReceived < 700 ? '8' : '6';
-    return node.isLeader ? (
-        <g id={`node=${address}`} onMouseOver={onMouseOver}>
-            <linearGradient id={`gradient-${address}`} key={`gradient-${address}`} fx="60%" fy="40%">
-                <stop stopColor={nodeColors[nodeState]} offset="75%"/>
-                <stop stopColor="#999" offset="100%"/>
-            </linearGradient>
-            <rect fill={`url(#gradient-${address})`} key={`circle-${address}`} x={cx - r} y={cy - r} height={r*2} width={r*2}/>
-        </g>
-
-    ) : (
+    return (
         <g id={`node-${address}`} onMouseOver={onMouseOver}>
             <radialGradient id={`gradient-${address}`} key={`gradient-${address}`} fx="60%" fy="40%">
                 <stop stopColor={nodeColors[nodeState]} offset="75%"/>
@@ -23,6 +15,7 @@ const Node = ({node, onMouseOver, selected}) => {
             </radialGradient>
             <circle fill={`url(#gradient-${address})`} key={`circle-${address}`}  cx={cx} cy={cy} r={r}/>
             {selected && <circle fill='white' key={`circle-border=${address}`} cx={cx} cy={cy} r="2"/>}
+            {node.isLeader && <Star cx={cx} cy={cy} r={r} />}
         </g>
     )
 };
