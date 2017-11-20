9dfb9f41a1f5547da9b57aa1921340bd6133756f
dmitrymukhin <dmitry@bluzelle.com>
2017-11-20 10:53:29 -0800

    [KEP-97] Token passed in ETHERSCAN_IO_API_TOKEN environment variable.

--- bluzelled/inc/ethereum/EthereumApi.h
+++ bluzelled/inc/ethereum/EthereumApi.h
@@ -1,6 +1,7 @@
 #ifndef KEPLER_ETHEREUM_API_H_H
 #define KEPLER_ETHEREUM_API_H_H
 
+#include <cstdlib>
 
 #include <string>
 #include <strstream>
@@ -28,7 +29,7 @@ namespace http = boost::beast::http;
 
 class EthereumApi {
     const string host_ = "ropsten.etherscan.io";
-    const string token = "UK282CWFFISMIPUMTXJNCTVCBZTJ2TPPRN";
+    const string etherscan_io_token_environment_variable_name = "ETHERSCAN_IO_API_TOKEN";
 
     const string get_token_balance_by_token_contract_address_format =
             "/api?module=account&action=tokenbalance&contractaddress=%s&address=%s&tag=latest&apikey=%s";
@@ -36,12 +37,17 @@ class EthereumApi {
     string address_;
     tcp::resolver resolver_;
     tcp::socket sock_;
+    string token_;
 
 public:
     EthereumApi(string str_addr,
                 boost::asio::io_service &ios)
             : address_(std::move(str_addr)),
               resolver_(ios), sock_(ios) {
+
+        auto env = std::getenv(etherscan_io_token_environment_variable_name.c_str());
+        if (env != nullptr)
+            token_ = env;
     }
 
     double token_balance(const EthereumToken& t);
@@ -59,6 +65,8 @@ private:
 
     template<typename T> T get_field(const boost::property_tree::ptree &tuple,
                                      const string &name) const;
+
+    void check_token();
 };
 
 
