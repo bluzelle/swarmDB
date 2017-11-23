d9d71d867cc6469db07d12621174ef9e37f7141b
dmitrymukhin <dmitry@bluzelle.com>
2017-11-22 16:04:28 -0800

    [KEP-99] Command line options for daemon. Added unit tests.

---
+++ daemon/command_line_options/command_line_options_test.cpp
@@ -0,0 +1,102 @@
+#define BOOST_TEST_MODULE "BaseClassModule"
+
+#include <boost/range.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include "CommandLineOptions.h"
+
+
+//  --run_test=test_options_all
+BOOST_AUTO_TEST_CASE(test_options_all) {
+    char *argv[] =
+            {
+                    (char *) "~/bluzelle/test",
+                    (char *) "--address",
+                    (char *) "0x2ba35056580b505690c03dfb1df58bc6b6cd9f89",
+                    (char *) "--port",
+                    (char *) "58000",
+                    (char *) "--config",
+                    (char *) "~/.bluzellerc",
+            };
+
+    CommandLineOptions op;
+    op.parse(7, argv);
+
+    BOOST_CHECK_EQUAL(op.get_address(), "0x2ba35056580b505690c03dfb1df58bc6b6cd9f89");
+    BOOST_CHECK_EQUAL(op.get_port(), 58000);
+    BOOST_CHECK(op.get_config() == "~/.bluzellerc");
+}
+
+//  --run_test=test_options_missing_address
+BOOST_AUTO_TEST_CASE(test_options_missing_address) {
+    char *argv[] =
+            {
+                    (char *) "~/bluzelle/test",
+                    (char *) "--port",
+                    (char *) "58000",
+                    (char *) "--config",
+                    (char *) "~/.bluzellerc",
+            };
+
+    try
+        {
+        CommandLineOptions op;
+        op.parse(5, argv);
+
+        BOOST_CHECK_EQUAL(op.get_address(), "");
+        }
+    catch (...)
+        {
+        BOOST_CHECK(false);
+        }
+}
+
+//  --run_test=test_options_missing_port
+BOOST_AUTO_TEST_CASE(test_options_missing_port) {
+    char *argv[] =
+            {
+                    (char *) "~/bluzelle/test",
+                    (char *) "--address",
+                    (char *) "0x2ba35056580b505690c03dfb1df58bc6b6cd9f89",
+                    (char *) "--config",
+                    (char *) "~/.bluzellerc",
+            };
+
+    try
+        {
+        CommandLineOptions op;
+        op.parse(5, argv);
+
+        BOOST_CHECK_EQUAL(op.get_port(), 0);
+        }
+    catch (...)
+        {
+        BOOST_CHECK(false);
+        }
+}
+
+//  --run_test=test_options_missing_config
+BOOST_AUTO_TEST_CASE(test_options_missing_config) {
+    char *argv[] =
+            {
+                    (char *) "~/bluzelle/test",
+                    (char *) "--address",
+                    (char *) "0x2ba35056580b505690c03dfb1df58bc6b6cd9f89",
+                    (char *) "--port",
+                    (char *) "58000",
+            };
+
+    try
+        {
+        CommandLineOptions op;
+        op.parse(5, argv);
+
+        BOOST_CHECK_EQUAL(op.get_config(), "");
+        }
+    catch (...)
+        {
+        BOOST_CHECK(false);
+        }
+}
+
+
