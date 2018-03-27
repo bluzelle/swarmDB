5950aa37a623f4b6b034aa218f91731f311cb197
Ed Bruck <ed.bruck1@gmail.com>
2018-03-27 09:58:28 -0700

    DEV-9: Integrate googletest into our builds

--- daemon2/cmake/add_gmock_test.cmake
+++ daemon2/cmake/add_gmock_test.cmake
@@ -1,5 +1,6 @@
 function(add_gmock_test target)
     add_executable(${target} ${test_srcs})
+    add_dependencies(${target} googletest)
     target_link_libraries(${target} ${test_libs} ${GMOCK_BOTH_LIBRARIES} ${Boost_LIBRARIES} ${JSONCPP_LIBRARIES} pthread)
     target_include_directories(${target} PRIVATE ${JSONCPP_INCLUDE_DIRS})
     add_test(${target} ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${target})
