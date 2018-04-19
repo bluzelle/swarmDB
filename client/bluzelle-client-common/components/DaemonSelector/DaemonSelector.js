5aec9cbd3c3032f4b88951e3ab805bfdfc5e6cee
Monty Thibault <monty@bluzelle.com>
2018-04-19 14:31:00 -0700

    Added UUID field to CRUD app

--- client/bluzelle-client-common/components/DaemonSelector/DaemonSelector.js
+++ client/bluzelle-client-common/components/DaemonSelector/DaemonSelector.js
@@ -3,11 +3,19 @@ import Panel from 'react-bootstrap/lib/Panel'
 import Button from 'react-bootstrap/lib/Button'
 import Header from '../Header'
 
+const uuidv4 = require('uuid/v4');
+
+
 @observer
 export default class DaemonSelector extends Component {
-    // go() {
-    //     entryPointUrl.set(`${this.address.value}:${this.port.value}`);
-    // }
+
+    go() {
+
+        const ws_url = 'ws://' + this.address.value + ':' + this.port.value;
+        const uuid = this.uuid.value;
+
+        this.props.go(ws_url, uuid);
+    }
 
     checkEnterKey(ev) {
         ev.keyCode === 13 && this.go();
@@ -47,8 +55,12 @@ export default class DaemonSelector extends Component {
                                 <label style={{display: 'block'}}>Address:</label>
                                 <input type="text" tabIndex="1" ref={r => this.address = r} style={{width: '80%'}} placeholder="address" defaultValue="127.0.0.1"/>
                             </div>
+                            <div style={{width: '100%'}}>
+                                <label style={{display: 'block'}}>UUID:</label>
+                                <input type="text" tabIndex="1" ref={r => this.uuid = r} style={{width: '100%'}} placeholder="uuid" defaultValue={uuidv4()}/>
+                            </div>
                             <div style={{marginTop: 10}}>
-                                    <Button onClick={this.props.go} tabIndex="3">Go</Button>
+                                    <Button onClick={this.go.bind(this)} tabIndex="3">Go</Button>
                                 {global.electron && <Button style={{marginLeft: 10}} disabled={emulatorStarted} onClick={this.startEmulator.bind(this)}>Start Emulator</Button>}
                             </div>
                         </div>
