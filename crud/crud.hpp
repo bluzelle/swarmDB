b0a384309200caeb30ca33771cd15a3ea9187419
Rich Nistuk <rnistuk@users.noreply.github.com>
2018-05-15 09:07:36 -0700

    KEP-276 Refactor CRUD Library (#29)
    * Refactoring CRUD to make it easier to understand and reason about

--- crud/crud.hpp
+++ crud/crud.hpp
@@ -35,40 +35,48 @@ namespace bzn
 
         void set_leader_info(bzn::message& msg);
 
-        void do_raft_task_routing(const bzn::message &msg, bzn::message &response);
+        void do_raft_task_routing(const bzn::message& msg, bzn::message& response);
 
-        bzn::message do_candidate_tasks(const bzn::message& msg);
-        bzn::message do_follower_tasks(const bzn::message& msg);
-        bzn::message do_leader_tasks(const bzn::message& msg);
+        void do_candidate_tasks(const bzn::message& request, bzn::message& response);
+        void do_follower_tasks(const bzn::message& request, bzn::message& response);
+        void do_leader_tasks(const bzn::message& request, bzn::message& response);
 
-        void leader_delete_task(const bzn::message &msg, bzn::message &response);
-        void handle_create_command(const bzn::message& msg, bzn::message& response);
-        void handle_update_command(const bzn::message& msg, bzn::message& response);
-
-        void handle_create(const bzn::message& msg);
+        void handle_create(const bzn::message& msg, bzn::message& response);
         void handle_read(const bzn::message& msg, bzn::message& response);
-        void handle_update(const bzn::message& msg);
-        void handle_delete(const bzn::message& msg);
+        void handle_update(const bzn::message& request, bzn::message& response);
+        void handle_delete(const bzn::message& request, bzn::message& response);
+
+        void commit_create(const bzn::message& msg);
+        void commit_update(const bzn::message& msg);
+        void commit_delete(const bzn::message& msg);
 
         // TODO move get keys out of crud.
         void handle_get_keys(const bzn::message& msg, bzn::message& response);
         void handle_has(const bzn::message& msg, bzn::message& response);
         void handle_size(const bzn::message& msg, bzn::message& response);
 
+        void register_route_handlers();
+        void register_crud_command_handlers();
+        void register_utility_command_handlers();
+        void register_commit_handlers();
+
+        bool validate_create_or_update(const bzn::message& request);
+        bool validate_read_or_delete(const bzn::message& request);
+
         std::shared_ptr<bzn::raft_base>    raft;
         std::shared_ptr<bzn::node_base>    node;
         std::shared_ptr<bzn::storage_base> storage;
 
-        using crud_handler_t = std::function<void(const bzn::message& msg)>;
+        using route_handler_t = std::function<void(const bzn::message& request, bzn::message& response)>;
+        using commit_handler_t = std::function<void(const bzn::message& msg)>;
+        using command_handler_t = std::function<void(const bzn::message& msg, bzn::message& resp)>;
 
-        using commit_handler_t = std::function<void(const bzn::message& msg, bzn::message& resp)>;
-
-        std::unordered_map<std::string, crud_handler_t> handlers;
-
-        std::unordered_map<std::string, commit_handler_t> command_handlers;
+        std::unordered_map<bzn::raft_state, route_handler_t> route_handlers;
+        std::unordered_map<std::string, commit_handler_t> commit_handlers;
+        std::unordered_map<std::string, command_handler_t> command_handlers;
+        std::unordered_map<std::string, command_handler_t> utility_handlers;
 
         std::once_flag start_once;
-
     };
 
 } // bzn
