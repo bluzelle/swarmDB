ec89ecb13b3b5e1ef95cb1d85cb260f77d074a81
Ed Bruck <ed.bruck1@gmail.com>
2018-08-28 12:10:42 -0700

    KEP-564 Add RocksDB to build

--- cmake/googletest.cmake
+++ cmake/googletest.cmake
@@ -1,11 +1,30 @@
+# Copyright (C) 2018 Bluzelle
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License, version 3,
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 include(ExternalProject)
 
 ExternalProject_Add(googletest
-        PREFIX "${CMAKE_CURRENT_BINARY_DIR}/googletest"
-        GIT_REPOSITORY https://github.com/google/googletest.git
-        INSTALL_COMMAND ""
-        GIT_TAG release-1.8.0
-        GIT_SHALLOW true)
+    PREFIX "${CMAKE_CURRENT_BINARY_DIR}/googletest"
+    URL https://github.com/google/googletest/archive/release-1.8.0.tar.gz
+    URL_HASH SHA256=58a6f4277ca2bc8565222b3bbd58a177609e9c488e8a72649359ba51450db7d8
+    TIMEOUT 30
+    INSTALL_COMMAND ""
+    DOWNLOAD_NO_PROGRESS true
+    )
+
+set_property(DIRECTORY PROPERTY CLEAN_NO_CUSTOM
+    "${CMAKE_CURRENT_BINARY_DIR}/googletest")
 
 ExternalProject_Get_Property(googletest source_dir)
 include_directories(${source_dir}/googlemock/include ${source_dir}/googletest/include)
--- cmake/swarm_version.cmake
+++ cmake/swarm_version.cmake
@@ -1,3 +1,17 @@
+# Copyright (C) 2018 Bluzelle
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License, version 3,
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 # version for swarm
 set(PROJECT_VERSION_MAJOR 0 CACHE STRING "Set the project major version")
 set(PROJECT_VERSION_MINOR 0 CACHE STRING "Set the project minor version")
