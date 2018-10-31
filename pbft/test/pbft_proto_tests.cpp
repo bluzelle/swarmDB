c04e9a82a1758b1297abb6341ee9b550d2bff0fe
Paul Archard <paul@bluzelle.com>
2018-10-31 15:38:02 -0700

    KEP-613: unit test fixture code for generating checkpoints

--- pbft/test/pbft_proto_tests.cpp
+++ pbft/test/pbft_proto_tests.cpp
@@ -47,11 +47,12 @@ namespace bzn
             // sending the initial request from a client
             auto request = new pbft_request();
             request->set_type(PBFT_REQ_DATABASE);
-            database_msg dmsg;
+            auto dmsg = new database_msg;
             auto create = new database_create;
             create->set_key(std::string("key_" + std::to_string(++this->index)));
             create->set_value(std::string("value_" + std::to_string(++this->index)));
-            dmsg.set_allocated_create(create);
+            dmsg->set_allocated_create(create);
+            request->set_allocated_operation(dmsg);
 
             bzn::json_message empty_json_msg;
             pbft->handle_request(*request, empty_json_msg);
@@ -102,18 +103,50 @@ namespace bzn
         // send fake commits from all nodes to SUT
         void send_commits(std::shared_ptr<pbft_operation> operation)
         {
+            // after commits are sent, SUT will post the operation for execution
+            // we want to simulate that it's been executed successfully
+            EXPECT_CALL(*(this->mock_io_context), post(_)).Times(Exactly(1))
+                .WillRepeatedly(Invoke([&](auto)
+                {
+                    //this->service_execute_handler(operation->request, operation->sequence);
+                }));
+
             for (const auto peer : TEST_PEER_LIST)
             {
                 pbft_msg commit;
 
                 commit.set_view(this->view);
                 commit.set_sequence(operation->sequence);
-                commit.set_type(PBFT_MSG_PREPARE);
+                commit.set_type(PBFT_MSG_COMMIT);
                 commit.set_allocated_request(new pbft_request(operation->request));
                 auto wmsg = wrap_pbft_msg(commit);
                 wmsg.set_sender(peer.uuid);
                 pbft->handle_message(commit, wmsg);
             }
+
+            // tell pbft that this operation has been executed
+            this->service_execute_handler(operation->request, operation->sequence);
+        }
+
+        void prepare_for_checkpoint(size_t seq)
+        {
+            // pbft needs a hash for this checkpoint
+            EXPECT_CALL(*this->mock_service, service_state_hash(seq)).Times(Exactly(1))
+                .WillRepeatedly(Invoke([&](auto)
+                {
+                    return std::to_string(seq);
+                }));
+
+            // after enough commits are sent, SUT will send out checkpoint message to all nodes
+            EXPECT_CALL(*this->mock_node, send_message_str(_, ResultOf(test::is_checkpoint, Eq(true)))).Times(
+                Exactly(TEST_PEER_LIST.size()));
+
+        }
+
+        void force_checkpoint(size_t seq)
+        {
+            prepare_for_checkpoint(seq);
+            this->pbft->checkpoint_reached_locally(seq);
         }
 
         void run_transaction_through_primary()
@@ -150,7 +183,18 @@ namespace bzn
     TEST_F(pbft_proto_test, init)
     {
         this->build_pbft();
+
+#if 0
+        for (size_t i = 0; i < 99; i++)
+            run_transaction_through_primary();
+        prepare_for_checkpoint(100);
         run_transaction_through_primary();
+#else
+        for (size_t i = 0; i < 9; i++)
+            run_transaction_through_primary();
+        force_checkpoint(10);
+        run_transaction_through_primary();
+#endif
     }
 }
 
