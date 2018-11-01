94bca7d803ba4e817300e40d7598278ca13400a0
Rich Nistuk <rnistuk@gmail.com>
2018-11-01 15:27:35 -0700

    KEP-332 Moved pbft_replica_sends_viewchange_message to the pbft_newview_test module, using the new unit test tools

--- pbft/test/pbft_newview_test.cpp
+++ pbft/test/pbft_newview_test.cpp
@@ -15,6 +15,7 @@
 #include <mocks/mock_boost_asio_beast.hpp>
 #include <mocks/mock_node_base.hpp>
 #include <mocks/mock_session_base.hpp>
+#include <pbft/test/pbft_proto_test.hpp>
 
 namespace
 {
@@ -24,9 +25,57 @@ namespace
 
 namespace bzn
 {
-    TEST(viewchange, test)
-    {
 
+    class pbft_viewchange_test : public pbft_proto_test
+    {
+    public:
+
+        size_t
+        max_faulty_replicas_allowed() { return TEST_PEER_LIST.size() / 3; }
+
+        void
+        send_f_plus_one_viewchange_messages()
+        {
+            size_t count{0};
+            const uint64_t NON_FAULTY_REPLICAS = TEST_PEER_LIST.size() / 3;
+
+            EXPECT_CALL(*mock_node, send_message_str(_, ResultOf(is_viewchange, Eq(true))))
+                    .Times(Exactly(TEST_PEER_LIST.size()))
+                    .WillRepeatedly(Invoke([&](auto &, auto &) { EXPECT_EQ((NON_FAULTY_REPLICAS + 1), count); }));
+
+
+            const size_t NEW_VIEW = this->pbft->get_view() + 1;
+            pbft_msg pbft_msg;
+            pbft_msg.set_type(PBFT_MSG_VIEWCHANGE);
+            pbft_msg.set_view(NEW_VIEW);
+
+            // let's pretend that the sytem under test is receiving view change messages
+            // from the other replicas
+            for (const auto &peer : TEST_PEER_LIST)
+            {
+                count++;
+                pbft_msg.set_sender(peer.uuid);
+                this->pbft->handle_message(pbft_msg, this->default_original_msg);
+                LOG(debug) << "\t***this->pbft->get_view(): " << this->pbft->get_view();
+                if (count == (NON_FAULTY_REPLICAS + 1))
+                    break;
+            }
+
+
+        }
+
+
+    };
+
+
+
+    TEST_F(pbft_viewchange_test, pbft_replica_sends_viewchange_message)
+    {
+        this->uuid = SECOND_NODE_UUID;
+        this->build_pbft();
+        this->send_f_plus_one_viewchange_messages();
     }
+
+
 
 }
