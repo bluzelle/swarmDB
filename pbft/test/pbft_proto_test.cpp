a2531dd530e83577c0c01cba91935c30afcfbea2
Rich Nistuk <rnistuk@gmail.com>
2018-12-06 14:01:33 -0800

    KEP-332 Fixes from Paul's review - returning comments that I had removed

--- pbft/test/pbft_proto_test.cpp
+++ pbft/test/pbft_proto_test.cpp
@@ -67,6 +67,7 @@ namespace bzn
     void
     pbft_proto_test::send_preprepare(uint64_t sequence, const bzn_envelope& request)
     {
+        // after preprepare is sent, SUT will send out prepares to all nodes
         EXPECT_CALL(*this->mock_node, send_message(_, ResultOf(test::is_prepare, Eq(true))))
             .Times(Exactly(TEST_PEER_LIST.size()));
 
