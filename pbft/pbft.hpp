108771ad2ca5a77652636d6c1ba535dbab64c20c
Rich Nistuk <rnistuk@users.noreply.github.com>
2018-12-14 21:48:18 -0800

    KEP-902 Fixed a exception that occurs in is_valid_viewchange_message … (#201)
    * KEP-902 Fixed a exception that occurs in is_valid_viewchange_message when no checkpoint has been set
    
    * KEP-902 Fixed a exception that occurs in is_valid_viewchange_message when no checkpoint has been set
    - re-did the fix and made the unit test check that the exception is not thrown
    - snuck in a fix to compare the viewchange_message.sequence to the valid_checkpoint_sequence after discussion with Isabel

--- pbft/pbft.hpp
+++ pbft/pbft.hpp
@@ -273,6 +273,7 @@ namespace bzn
         FRIEND_TEST(pbft_viewchange_test, test_fill_in_missing_pre_prepares);
         FRIEND_TEST(pbft_viewchange_test, test_save_checkpoint);
         FRIEND_TEST(pbft_viewchange_test, test_handle_viewchange);
+        FRIEND_TEST(pbft_viewchange_test, is_valid_viewchange_does_not_throw_if_no_checkpoint_yet);
 
         FRIEND_TEST(pbft_newview_test, test_pre_prepares_contiguous);
         FRIEND_TEST(pbft_newview_test, make_newview);
