440bde1c4f929a2580b2d48da0eda10f9a1d527c
Paul Archard <paul@bluzelle.com>
2019-03-27 13:33:12 -0700

    KEP-1294: missing lock causing instability of swarm

--- pbft/pbft.cpp
+++ pbft/pbft.cpp
@@ -268,7 +268,7 @@ pbft::preliminary_filter_msg(const pbft_msg& msg)
 
         if (msg.sequence() <= this->low_water_mark)
         {
-            LOG(debug) << boost::format("Dropping message because sequence number %1% less than %2%") % msg.sequence()
+            LOG(debug) << boost::format("Dropping message because sequence number %1% <= %2%") % msg.sequence()
                 % this->low_water_mark;
             return false;
         }
@@ -325,7 +325,7 @@ pbft::handle_request(const bzn_envelope& request_env, const std::shared_ptr<sess
     if (this->already_seen_request(request_env, hash))
     {
         // TODO: send error message to client
-        LOG(info) << "Rejecting duplicate request: " << request_env.ShortDebugString();
+        LOG(info) << "Rejecting duplicate request: " << request_env.ShortDebugString().substr(0, MAX_MESSAGE_SIZE);
         return;
     }
     this->saw_request(request_env, hash);
@@ -1101,6 +1101,8 @@ pbft::handle_database_message(const bzn_envelope& msg, std::shared_ptr<bzn::sess
     {
         mutable_msg.set_timestamp(this->now());
     }
+
+    std::lock_guard<std::mutex> lock(this->pbft_lock);
 
     LOG(debug) << "got database message";
 
