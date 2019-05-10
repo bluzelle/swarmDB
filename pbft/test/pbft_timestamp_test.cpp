19acf14a47d29c9de71b4723f18c62e00dfd8213
Ed Bruck <ed.bruck1@gmail.com>
2019-05-10 11:58:30 -0700

    KEP-1414 Fix pbft protobuf memory leaks

--- pbft/test/pbft_timestamp_test.cpp
+++ pbft/test/pbft_timestamp_test.cpp
@@ -26,29 +26,26 @@ namespace bzn
     {
         this->build_pbft();
 
-        auto create = new database_create;
-        create->set_key(std::string("key"));
-        create->set_value(std::string("value"));
+        database_msg dmsg;
+        dmsg.mutable_create()->set_key(std::string("key"));
+        dmsg.mutable_create()->set_value(std::string("value"));
 
-        auto dmsg = new database_msg;
-        dmsg->set_allocated_create(create);
-
-        auto request = new bzn_envelope();
-        request->set_database_msg(dmsg->SerializeAsString());
-        request->set_timestamp(this->now());
-        request->set_sender(TEST_NODE_UUID);
+        bzn_envelope request;
+        request.set_database_msg(dmsg.SerializeAsString());
+        request.set_timestamp(this->now());
+        request.set_sender(TEST_NODE_UUID);
 
         // the first time we should get pre-prepare messages
         EXPECT_CALL(*this->mock_node, send_signed_message(A<const boost::asio::ip::tcp::endpoint&>(), ResultOf(test::is_preprepare, Eq(true))))
             .Times(Exactly(TEST_PEER_LIST.size()));
-        this->handle_request(*request);
+        this->handle_request(request);
 
-        auto request2 = new bzn_envelope(*request);
+        auto request2 = bzn_envelope(request);
 
         // this time no pre-prepare should be issued
         EXPECT_CALL(*this->mock_node, send_signed_message(A<const boost::asio::ip::tcp::endpoint&>(), ResultOf(test::is_preprepare, Eq(true))))
             .Times(Exactly(0));
-        this->handle_request(*request2);
+        this->handle_request(request2);
     }
 
     TEST_F(pbft_proto_test, similar_request_generates_preprepare)
@@ -56,48 +53,43 @@ namespace bzn
         this->build_pbft();
 
         // send an initial message
-        auto create = new database_create;
-        create->set_key(std::string("key"));
-        create->set_value(std::string("value"));
+        database_msg dmsg;
+        dmsg.mutable_create()->set_key(std::string("key"));
+        dmsg.mutable_create()->set_value(std::string("value"));
 
-        auto dmsg = new database_msg;
-        dmsg->set_allocated_create(create);
-
-        auto request = new bzn_envelope();
-        request->set_database_msg(dmsg->SerializeAsString());
-        request->set_timestamp(this->now());
-        request->set_sender(TEST_NODE_UUID);
+        bzn_envelope request;
+        request.set_database_msg(dmsg.SerializeAsString());
+        request.set_timestamp(this->now());
+        request.set_sender(TEST_NODE_UUID);
 
         // we should get pre-prepare messages
         EXPECT_CALL(*this->mock_node, send_signed_message(A<const boost::asio::ip::tcp::endpoint&>(), ResultOf(test::is_preprepare, Eq(true))))
             .Times(Exactly(TEST_PEER_LIST.size()));
-        this->handle_request(*request);
+        this->handle_request(request);
 
 
         // send a second message the same as first but with a slightly different timestamp
-        auto request2 = new bzn_envelope(*request);
-        request2->set_timestamp(request2->timestamp() + 1);
+        auto request2 = bzn_envelope(request);
+        request2.set_timestamp(request2.timestamp() + 1);
 
         // again we should get pre-prepare messages
         EXPECT_CALL(*this->mock_node, send_signed_message(A<const boost::asio::ip::tcp::endpoint&>(), ResultOf(test::is_preprepare, Eq(true))))
             .Times(Exactly(TEST_PEER_LIST.size()));
-        this->handle_request(*request2);
+        this->handle_request(request2);
 
 
         // send a third message the same as first but with same timestamp and different operation
-        auto create3 = new database_create;
-        create3->set_key(std::string("key3"));
-        create3->set_value(std::string("value3"));
+        database_msg dmsg3;
+        dmsg3.mutable_create()->set_key(std::string("key3"));
+        dmsg3.mutable_create()->set_value(std::string("value3"));
 
-        auto dmsg3 = new database_msg;
-        dmsg3->set_allocated_create(create3);
-        auto request3 = new bzn_envelope(*request);
-        request3->set_database_msg(dmsg3->SerializeAsString());
+        auto request3 = bzn_envelope(request);
+        request3.set_database_msg(dmsg3.SerializeAsString());
 
         // again we should get pre-prepare messages
         EXPECT_CALL(*this->mock_node, send_signed_message(A<const boost::asio::ip::tcp::endpoint&>(), ResultOf(test::is_preprepare, Eq(true))))
             .Times(Exactly(TEST_PEER_LIST.size()));
-        this->handle_request(*request3);
+        this->handle_request(request3);
     }
 
     TEST_F(pbft_proto_test, same_request_from_different_client_generates_preprepare)
@@ -105,53 +97,47 @@ namespace bzn
         this->build_pbft();
 
         // send an initial message
-        auto create = new database_create;
-        create->set_key(std::string("key"));
-        create->set_value(std::string("value"));
+        database_msg dmsg;
+        dmsg.mutable_create()->set_key(std::string("key"));
+        dmsg.mutable_create()->set_value(std::string("value"));
 
-        auto dmsg = new database_msg;
-        dmsg->set_allocated_create(create);
-
-        auto request = new bzn_envelope();
-        request->set_database_msg(dmsg->SerializeAsString());
-        request->set_timestamp(this->now());
-        request->set_sender(TEST_NODE_UUID);
+        bzn_envelope request;
+        request.set_database_msg(dmsg.SerializeAsString());
+        request.set_timestamp(this->now());
+        request.set_sender(TEST_NODE_UUID);
 
         // we should get pre-prepare messages
         EXPECT_CALL(*this->mock_node, send_signed_message(A<const boost::asio::ip::tcp::endpoint&>(), ResultOf(test::is_preprepare, Eq(true))))
             .Times(Exactly(TEST_PEER_LIST.size()));
-        this->handle_request(*request);
+        this->handle_request(request);
 
         // send a second message the same as first but from different client
-        auto request2 = new bzn_envelope(*request);
-        request2->set_sender(SECOND_NODE_UUID);
+        auto request2 = bzn_envelope(request);
+        request2.set_sender(SECOND_NODE_UUID);
 
         // again we should get pre-prepare messages
         EXPECT_CALL(*this->mock_node, send_signed_message(A<const boost::asio::ip::tcp::endpoint&>(), ResultOf(test::is_preprepare, Eq(true))))
             .Times(Exactly(TEST_PEER_LIST.size()));
-        this->handle_request(*request2);
+        this->handle_request(request2);
     }
 
     TEST_F(pbft_proto_test, old_request_is_rejected)
     {
         this->build_pbft();
 
-        auto create = new database_create;
-        create->set_key(std::string("key"));
-        create->set_value(std::string("value"));
+        database_msg dmsg;
+        dmsg.mutable_create()->set_key(std::string("key"));
+        dmsg.mutable_create()->set_value(std::string("value"));
 
-        auto dmsg = new database_msg;
-        dmsg->set_allocated_create(create);
-
-        auto request = new bzn_envelope();
-        request->set_database_msg(dmsg->SerializeAsString());
-        request->set_timestamp(0);
-        request->set_sender(TEST_NODE_UUID);
+        bzn_envelope request;
+        request.set_database_msg(dmsg.SerializeAsString());
+        request.set_timestamp(0);
+        request.set_sender(TEST_NODE_UUID);
 
         // we should NOT get pre-prepare messages since this is an old request
         EXPECT_CALL(*this->mock_node, send_signed_message(A<const boost::asio::ip::tcp::endpoint&>(), ResultOf(test::is_preprepare, Eq(true))))
             .Times(Exactly(0));
-        this->handle_request(*request);
+        this->handle_request(request);
     }
 
     TEST_F(pbft_proto_test, range_test)
